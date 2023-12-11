57ab0f704c6b8c01bf22fed7a4ec5d56ad5bb81c
Tolga Yildiz <Tolga.Yildiz@suratkargo.com.tr>
2023-12-11 15:50:40 +0300

    testimonial

---
+++ CarBook/Core/CarBook.Application/Features/Mediator/Handlers/TestimonialHandlers/UpdateTestimonialCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using CarBook.Application.Features.Mediator.Commands.TestimonialCommands;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Handlers.TestimonialHandlers
+{
+    public class UpdateTestimonialCommandHandler : IRequestHandler<UpdateTestimonialCommand>
+    {
+        private readonly IRepository<Testimonial> _repository;
+
+        public UpdateTestimonialCommandHandler(IRepository<Testimonial> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateTestimonialCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _repository.GetByIdAsync(request.TestimonialID);
+            entity.Title = request.Title;
+            entity.Comment = request.Comment;
+            entity.ImageUrl = request.ImageUrl;
+            entity.Name = request.Name;
+            await _repository.UpdateAsync(entity);
+        }
+    }
+}
