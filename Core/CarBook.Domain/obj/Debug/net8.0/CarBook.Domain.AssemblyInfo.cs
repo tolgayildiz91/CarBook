028daa2f7dc9ca9c7123355b24a67e0de2c3356f
Tolga Yildiz <Tolga.Yildiz@suratkargo.com.tr>
2023-12-20 16:59:53 +0300

    .

--- Core/CarBook.Domain/obj/Debug/net8.0/CarBook.Domain.AssemblyInfo.cs
+++ Core/CarBook.Domain/obj/Debug/net8.0/CarBook.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarBook.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57ab0f704c6b8c01bf22fed7a4ec5d56ad5bb81c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarBook.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarBook.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
