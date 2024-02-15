351548d8f95f139562b2fdad68c5f2be302de6d7
Tolga Yildiz <Tolga.Yildiz@suratkargo.com.tr>
2024-02-15 08:42:18 +0300

    admincar createcar

--- CarBook/Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
+++ CarBook/Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
@@ -1,7 +1,9 @@
-﻿using CarBook.DTO.CarDtos;
+﻿using CarBook.DTO.BrandDtos;
+using CarBook.DTO.CarDtos;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace CarBook.WebUI.Controllers
 {
@@ -18,14 +20,30 @@ namespace CarBook.WebUI.Controllers
         {
             var client = _httpClientFactory.CreateClient();
             var responseMessage = await client.GetAsync("https://localhost:7052/api/GetCarWithBrand");
-            if(responseMessage.IsSuccessStatusCode)
+            if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<List<ResultCarWithBrandsDtos>>(jsonData);
                 return View(values);
-
             }
+            return View();
+        }
 
+        [HttpGet]
+        public async Task<IActionResult> CreateCar()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7052/api/Brands");
+
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultBrandDto>>(jsonData);
+            List<SelectListItem> brandValues = (from x in values
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.name,
+                                                    Value = x.brandID.ToString()
+                                                }).ToList();
+            ViewBag.BrandValues = brandValues;
             return View();
         }
     }
