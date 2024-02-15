c402af5b03f5ec1d2754c612bc7858e1ae02fcef
Tolga Yildiz <Tolga.Yildiz@suratkargo.com.tr>
2024-02-15 08:30:39 +0300

    admincar index

--- CarBook/Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
+++ CarBook/Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
@@ -1,11 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.DTO.CarDtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
 
 namespace CarBook.WebUI.Controllers
 {
     public class AdminCarController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminCarController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7052/api/GetCarWithBrand");
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCarWithBrandsDtos>>(jsonData);
+                return View(values);
+
+            }
+
             return View();
         }
     }
